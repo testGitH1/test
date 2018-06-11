580ae302cbeb27be382f9c480ea87156d38c4324
testGitH1 <40166602+testGitH1@users.noreply.github.com>
2018-06-11 18:41:35 +0530

    Update ChildClass.java

--- RestAssured/src/com/learn/core/concepts/ChildClass.java
+++ RestAssured/src/com/learn/core/concepts/ChildClass.java
@@ -7,7 +7,7 @@ public class ChildClass extends ParentClass
 	public ChildClass() throws InterruptedException 
 	{
 		System.out.println("ChildClass Constructor");
-			System.out.println("This line changed in git hub");
+		System.out.println("This line changed in git hub");
 		Thread localThread= Thread.currentThread();
 		System.err.println(localThread.getId());
 		System.out.println(localThread.getName());
